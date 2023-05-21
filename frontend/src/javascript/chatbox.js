7f4bb5485a2f7c86e60a1cda8e7d8d76b5904734
white-dough <dwight.eyac20@gmail.com>
2023-05-21 13:23:53 +0800

    complete user chat side -send messages -receive messages (every 1 sec)

--- frontend/src/javascript/chatbox.js
+++ frontend/src/javascript/chatbox.js
@@ -1,17 +1,17 @@
-// //chatbox click open
-// var chatCircleClick = document.getElementById('chat-circle');
-// var chatModal = document.getElementById('chat-box');
-// function openChatBox(){
-//   chatModal.style.display = 'block';
-// }
-// chatCircleClick.addEventListener('click', openChatBox);
+//chatbox click open
+var chatCircleClick = document.getElementById('chat-circle');
+var chatModal = document.getElementById('chat-box');
+function openChatBox(){
+  chatModal.style.display = 'block';
+}
+chatCircleClick.addEventListener('click', openChatBox);
 
-// //chatbox close
-// var chatClose = document.getElementById('chat-box-toggle');
-// function closeChatBox(){
-//   chatModal.style.display = 'none';
-// }
-// chatClose.addEventListener('click', closeChatBox);
+//chatbox close
+var chatClose = document.getElementById('chat-box-toggle');
+function closeChatBox(){
+  chatModal.style.display = 'none';
+}
+chatClose.addEventListener('click', closeChatBox);
 
 
 //send message
@@ -29,8 +29,39 @@ function sendMessage() {
         console.log(xhr.responseText);
       }
     };
-    xhr.send('  message=' + encodeURIComponent(message));
+    xhr.send('message=' + encodeURIComponent(message));
   }
 }
 
 sendButton.addEventListener('click', sendMessage);
+
+var chatIDRendered = [];
+
+//receive incoming chats
+function receiveMessage() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {    
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var receivedMessages = JSON.parse(xhr.responseText);        
+      var container = document.getElementById("received-message");
+      receivedMessages.forEach(element => {
+        if (chatIDRendered.indexOf(element['chatID']) == -1){
+          var receivedMessage = document.createElement("div");
+          receivedMessage.id = "dynamicDiv";
+          receivedMessage.textContent  = element['chatMessage'];
+          container.appendChild(receivedMessage);
+          chatIDRendered.push(element['chatID']);
+        }
+        
+        console.log(chatIDRendered);
+      });
+      // for (var i = 0; i < receivedMessages.length; i++) {
+        
+      // }
+      
+    }
+  };  
+  xhr.open('GET', '../pages/chat-process.php', true);
+  xhr.send();
+}
+setInterval(receiveMessage, 1000);
--- frontend/src/pages/chat-process.php
+++ frontend/src/pages/chat-process.php
@@ -1,9 +1,26 @@
 <?php 
     include('config.php');
-    include('loginprocess.php');
-    $message = $_POST['message'];
+    include('loginprocess.php');    
     $userLoggedIn = $_SESSION['loggedInUser'];
     $senderUID = $userLoggedIn['userID'];
-    $sql = mysqli_query($mysqli,"INSERT INTO `user_chat_message` (`senderUID`,`chatMessage`,`sentDate`) VALUES ('$senderUID','$message', current_timestamp())");
+    $receiveMessage = [];
+    //sending
+    if (isset($_POST['message'])){
+        $message = $_POST['message'];
+        $sql = mysqli_query($mysqli,"INSERT INTO `user_chat_message` (`senderUID`,`chatMessage`,`sentDate`) VALUES ('$senderUID','$message', current_timestamp())");    
+    }
+    
+    //receiving
+    $sql = mysqli_query($mysqli,"SELECT * FROM `staff_chat_message` WHERE `receiverUID` = '$senderUID'");
+    if ($sql){
+        $results = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+        foreach ($results as $result){  
+            array_push($receiveMessage, array("chatID" => $result['chatID'], "chatMessage" => $result['chatMessage']));
+        }        
+        echo json_encode($receiveMessage);
+    }
+    else{
+        echo "Connection Failure. Please refresh the page.";
+    }    
 
 ?>
