91b6b1561d19d5e8caa4e73ff6d04c4f5529974b
Moses <mosesfat@gmail.com>
2023-05-23 13:09:11 +0800

    chat changes

--- frontend/src/javascript/chatbox.js
+++ frontend/src/javascript/chatbox.js
@@ -57,15 +57,41 @@ function receiveMessage() {
           switch (element['SentOrReceived']) {
             case "received":
               var receivedMessage = document.createElement("div");
+              receivedContainer.className = "flex justify-end"
               receivedMessage.id = "receive-msg";
-              receivedMessage.textContent  = element['chatMessage'];
+              receivedMessage.className = "items-end bg-gray-300 text-start p-2"
+              receivedMessage.style.border = "1px solid gray";
+              receivedMessage.style.display = "inline-block"; // Set the display to inline-block
+              receivedMessage.textContent = element['chatMessage'];
+              
+              // Temporarily append the div to the body to calculate the text width
+              document.body.appendChild(receivedMessage);
+              var textWidth = receivedMessage.offsetWidth;
+              document.body.removeChild(receivedMessage);
+              
+              receivedMessage.style.width = textWidth + "px"; // Set the width based on the text width
+              
               receivedContainer.appendChild(receivedMessage);
+              receivedContainer.appendChild(document.createElement("br"));
               break;            
-              case "sent":
-                var sentMessage = document.createElement("div");
-                sentMessage.id = "sent-msg";
-                sentMessage.textContent  = element['chatMessage'];
-                sentContainer.appendChild(sentMessage);
+            case "sent":
+              var sentMessage = document.createElement("div");
+              sentMessage.id = "sent-msg";
+              sentMessage.className = "flex justify-end bg-blue-300 p-2"
+              sentMessage.style.border = "1px solid #32A9DD";
+              sentMessage.style.display = "inline-block";
+              sentMessage.style.textAlign = "right"; // Set the display to inline-block
+              sentMessage.textContent = element['chatMessage'];
+              
+              // Temporarily append the div to the body to calculate the text width
+              document.body.appendChild(sentMessage);
+              var textWidth = sentMessage.offsetWidth;
+              document.body.removeChild(sentMessage);
+              
+              sentMessage.style.width = textWidth + "px"; // Set the width based on the text width
+              
+              sentContainer.appendChild(sentMessage);
+              sentContainer.appendChild(document.createElement("br"));
               break;          
             default:
               break;
